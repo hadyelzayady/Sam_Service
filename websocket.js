6a8dde3a37c884bbfa9aef36e83924e91a6278f3
hadyelzayady <hadyelzayady@yahoo.com>
2019-04-30 22:04:13 +0200

    receive reset and send it to board

--- websocket.js
+++ websocket.js
@@ -12,7 +12,14 @@ conn_socket = io.on('connect', function (socket) {
     });
     socket.on('message', function () {
         console.log('received message from websocket');
-        udp_server.send('hello', 1234, function (erro, bytes) {
+        udp_server.send("hello", 1234, function (erro, bytes) {
+            console.log("error:", erro)
+        })
+
+    });
+    socket.on('reset', function (ip, port) {
+        console.log('received reset from websocket:', ip, port);
+        udp_server.send("reset", port, ip, function (erro, bytes) {
             console.log("error:", erro)
         })
 
