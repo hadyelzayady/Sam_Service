6f02c20d2f38a7458f94dbfc93688ab6f58a736f
hadyelzayady <hadyelzayady@yahoo.com>
2019-05-24 01:25:46 +0200

    send to board sim value

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var MessageHeaders = require('./Constants').MessageHeader
 function parseSimData(packet) {
 
     binday_data = Buffer.from(packet, 0, 2)
-    console.log("binary data", binday_data)
+    // console.log("binary data", binday_data)
     pin_number = binday_data.readInt8(0)
     value = binday_data.readInt8(1)
-    console.log("VALUE:", value, " pin numbe", pin_number)
+    // console.log("VALUE:", value, " pin numbe", pin_number)
     return [pin_number, value]
 }
 
@@ -19,9 +19,9 @@ udp_server.on('message', (packet, rinfo) => {
     //    header,msg=messageParser(packet)
     try {
         // packet=
-        console.log("received message", packet)
+        // console.log("received message", packet)
         let [pin_number, value] = parseSimData(packet)
-        console.log(`server got: ${packet} from ${rinfo.address}:${rinfo.port}`);
+        // console.log(`server got: ${packet} from ${rinfo.address}:${rinfo.port}`);
 
         socket.emit('message',
             {
--- websocket.js
+++ websocket.js
@@ -10,8 +10,27 @@ conn_socket = io.on('connect', function (socket) {
         console.log('user disconnected');
     });
     socket.on('message', function (message) {
-        console.log('received message from websocket',message.value);
+        console.log('received message from websocket', message.value);
         udp_server.send("hello", 1234, function (erro, bytes) {
+            console.log("error:", erro)
+        })
+
+    });
+    socket.on('sendToBoard', function (sim_info) {
+        value = sim_info.value
+        IP = sim_info.IP
+        port = sim_info.port
+        pin_id = sim_info.pin_id
+        console.log("IP port", pin_id, value)
+        console.log('received send to board from websocket:', IP, port);
+        data = Buffer.alloc(2, 'pin')
+        data[0] = pin_id
+        data[1] = value
+        data.writeInt8(pin_id, 0)
+        value = value ? 1 : 0
+        data.writeInt8(value, 1)
+        console.log(data)
+        udp_server.send(data, 1919, IP, function (erro, bytes) {
             console.log("error:", erro)
         })
 
